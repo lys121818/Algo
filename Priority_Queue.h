b2a48fa55b7f3704752a97cb6df38836d6b885f5
Youngsung Lee <lys121818@gmail.com>
2023-11-21 20:47:10 -0800

    Priority_Queue

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <assert.h>
 #include <iostream>
-#include <queue>
+
+// Debug mode to see visual changes
+#define _DEBUG 1
 
 template<typename DataType>
 class Priority_Queue
@@ -20,14 +22,21 @@ private:
 		Node(size_t key, DataType data) : m_key(key), m_value(data) {}
 		~Node() 
 		{ 
+			// root
+			if (m_key == 1)
+				return;
+
+			//left
 			if (m_key % 2 == 0)
 				m_pParent->m_pLeft = nullptr;
+			//right
 			else
 				m_pParent->m_pRight = nullptr;
 
 			m_pParent = m_pLeft = m_pRight = nullptr; 
 		}
 
+		// Compare values
 		bool operator < (const Node& other) const
 		{
 			if (this->m_value < other.m_value) return true; 
@@ -43,7 +52,6 @@ private:
 
 	DataType m_maxElement;
 
-	std::queue<Node*> m_pQueue;
 public:
 
 
@@ -65,22 +73,31 @@ public:
 		void Left() { m_pNode = m_pNode->m_pLeft; }
 		void Right() { m_pNode = m_pNode->m_pRight; }
 
-		size_t GetKey() { return m_pNode->m_key; }
-
-		// return node
-		Node* operator*() { return m_pNode; }
-
+		Node* GetNode() { return m_pNode; }
 	};
 
 public:
 	Priority_Queue() = default;
-	~Priority_Queue() {};
+	~Priority_Queue() { Clear(m_pRoot); delete m_pRoot; }
 
 	// Inset data into queue
 	void Push(DataType data);
 
 	void Delete(size_t key);
+
+	void Print();
 private:
+
+	// print visualized tree in recursive
+	void Print(Node* pNode, int merge);
+
+	// delete all the child node in recursive
+	void Clear(Node* pNode);
+
+#if _DEBUG
+	void DebugPrint(Node* pNode_a = nullptr, Node* pNode_b = nullptr);
+#endif
+
 	size_t Parent(size_t index) const { return index / 2; }
 	size_t Left(size_t index) const { return index * 2; }
 	size_t Right(size_t index) const { return index * 2 + 1; }
@@ -91,12 +108,12 @@ private:
 		assert(index < m_size && m_pRoot != nullptr);
 
 		Iterator iter = m_pRoot;
-		while (iter.GetKey() != index)
+		while (iter.GetNode()->m_key != index)
 		{
 			size_t nextIndex = index;
 
-			size_t leftChildKey = Priority_Queue<DataType>::Left(iter.GetKey());
-			size_t rightChildKey = Priority_Queue<DataType>::Right(iter.GetKey());
+			size_t leftChildKey = iter.GetNode()->m_pLeft->m_key;
+			size_t rightChildKey = iter.GetNode()->m_pRight->m_key;
 
 			// devide in half until index is lower than current nodes biggest child
 			while (nextIndex > rightChildKey)
@@ -113,7 +130,7 @@ private:
 			else
 				iter.Right();
 		}
-		return *iter;
+		return iter.GetNode();
 	}
 
 	void SwapValues(Node* pNode_a, Node* pNode_b);
@@ -128,24 +145,24 @@ private:
 		Iterator iter = m_pRoot;
 
 		// Until find the index
-		while (iter.GetKey() != index)
+		while (iter.GetNode()->m_key != index)
 		{
 			size_t nextIndex = index;
 
 			// if left or right is nullptr (reach the last element)
-			if ((*iter)->m_pRight == nullptr)
+			if (iter.GetNode()->m_pRight == nullptr)
 			{
 				iter.Left();
 				break;
 			}
 			// left always exist
 			else
-				assert((*iter)->m_pLeft != nullptr);
+				assert(iter.GetNode()->m_pLeft != nullptr);
 
 			
 
-			size_t leftChildKey = (*iter)->m_pLeft->m_key;
-			size_t rightChildKey = (*iter)->m_pRight->m_key;
+			size_t leftChildKey = iter.GetNode()->m_pLeft->m_key;
+			size_t rightChildKey = iter.GetNode()->m_pRight->m_key;
 
 			// devide in half until index is lower than current nodes biggest child
 			while (nextIndex > rightChildKey)
@@ -162,8 +179,10 @@ private:
 				iter.Right();
 
 		}
-		return *iter;
+		return iter.GetNode();
 	}
+
+
 };
 
 template<typename DataType>
@@ -177,11 +196,16 @@ void Priority_Queue<DataType>::Push(DataType data)
 	// if Queue is empty
 	if (m_size == 1)
 	{
+
 		m_pRoot = newNode;
 		m_maxElement = newNode->m_value;
-		m_pQueue.push(newNode);
+
+#if _DEBUG
+		DebugPrint(newNode);
+#endif // DEBUG
 		return;
 	}
+
 
 	// connect with parent
 	size_t parentIndex = Parent(m_size);
@@ -194,13 +218,17 @@ void Priority_Queue<DataType>::Push(DataType data)
 	else
 		parentNode->m_pRight = newNode;
 
-	m_pQueue.push(newNode);
+#if _DEBUG
+	DebugPrint(newNode);
+#endif // DEBUG
 
 	// Sort in order
 	SortOnUp(newNode);
 
 	// Set max element
 	m_maxElement = m_pRoot->m_value;
+
+
 }
 
 template<typename DataType>
@@ -210,30 +238,52 @@ inline void Priority_Queue<DataType>::Delete(size_t key)
 	assert(key <= m_size);
 
 	/*
-		On this function pDeleteNode is actually deleting values 
+	 	On this function pDeleteNode is actually deleting values 
 		but after sorting is done pLastIndexNode is one actaully being deleted
-		Maybe better name for this?
 	*/
+
 	Node* pDeleteNode = Find(key);
 	Node* pLastIndexNode = Find(m_size);
 
+#if _DEBUG
+
+	system("cls");
+	std::cout << "Deleting Node[" << key << "]" << pDeleteNode->m_value << "\n\n";
+	Print();
+	system("pause");
+
+#endif // DEBUG
+
 	// set value to biggest
-	pDeleteNode->m_value = ++m_maxElement;
+	pDeleteNode->m_value = m_maxElement;
+
+#if _DEBUG
+
+	system("cls");
+	std::cout << "Change Value to maxElement Node[" << key << "]" << pDeleteNode->m_value << "\n\n";
+	Print();
+	system("pause");
+
+#endif // DEBUG
 
 	// sorting on the way to the root
 	SortOnUp(pDeleteNode);
 
-	//m_pQueue.pop();
-
 	// Swap
 	SwapValues(m_pRoot, pLastIndexNode);
 
+	// delete last element
 	delete pLastIndexNode;
 
 	pLastIndexNode = nullptr;
 
+#if _DEBUG
+	DebugPrint();
+#endif // DEBUG
+
 	--m_size;
 	
+	// sort down after moved last element to root
 	SortOnDown(m_pRoot);
 
 	// Set max element
@@ -241,42 +291,95 @@ inline void Priority_Queue<DataType>::Delete(size_t key)
 }
 
 template<typename DataType>
+inline void Priority_Queue<DataType>::Print()
+{
+	Print(m_pRoot,0);
+	std::cout << "\n\n";
+}
+
+template<typename DataType>
+inline void Priority_Queue<DataType>::Print(Node* pNode, int merge)
+{
+	merge += m_size;
+
+	if (pNode == nullptr)
+		return;
+
+	// Move to right end node
+	Print(pNode->m_pRight, merge);
+
+	std::cout << "\n";
+
+	for (int i = m_size; i < merge; ++i)
+		std::cout << " ";
+	std::cout << "[" << pNode->m_key << "]" << pNode->m_value;
+
+	Print(pNode->m_pLeft, merge);
+
+}
+
+template<typename DataType>
+inline void Priority_Queue<DataType>::Clear(Node* pNode)
+{
+	if (pNode == nullptr)
+		return;
+
+	// Move to very left node
+	Clear(pNode->m_pLeft);
+
+	// delete child
+	if (pNode->m_pLeft)
+		delete pNode->m_pLeft;
+	else if (pNode->m_pRight)
+		delete pNode->m_pRight;
+
+	// Move to right node
+	Clear(pNode->m_pRight);
+
+}
+
+template<typename DataType>
 inline void Priority_Queue<DataType>::SwapValues(Node* pNode_a, Node* pNode_b)
 {
+	// Swap values
 	DataType temp = pNode_a->m_value;
 	pNode_a->m_value = pNode_b->m_value;
 	pNode_b->m_value = temp;
+
+#if _DEBUG
+	DebugPrint(pNode_a, pNode_b);
+#endif // DEBUG
 }
 
 template<typename DataType>
 inline void Priority_Queue<DataType>::SortOnDown(Node* pNode)
 {
-	Node* searchingNode = pNode;
-
-	// Until there are no child
-	while (searchingNode->m_pLeft != nullptr)
+	Iterator iter = pNode;
+	
+	// Until there are no child on right (no more to go down)
+	while (iter.GetNode()->m_pRight != nullptr)
 	{
 		// when left child is bigger
-		if (*searchingNode->m_pLeft > *searchingNode)
+		if (*iter.GetNode()->m_pLeft > *iter.GetNode())
 		{
 			// when left child is bigger than right child
-			if (*searchingNode->m_pLeft > *searchingNode->m_pRight)
+			if (*iter.GetNode()->m_pLeft > *iter.GetNode()->m_pRight)
 			{
-				SwapValues(searchingNode->m_pLeft, searchingNode);
-				searchingNode = searchingNode->m_pLeft;
+				SwapValues(iter.GetNode()->m_pLeft, iter.GetNode());
+				iter.Left();
 			}
 			// when right child is bigger than left child
 			else
 			{
-				SwapValues(searchingNode->m_pRight, searchingNode);
-				searchingNode = searchingNode->m_pRight;
+				SwapValues(iter.GetNode()->m_pRight, iter.GetNode());
+				iter.Right();
 			}
 		}
 		// when right child is bigger
-		else if (*searchingNode->m_pRight > *searchingNode)
+		else if (*iter.GetNode()->m_pRight > *iter.GetNode())
 		{
-			SwapValues(searchingNode->m_pRight, searchingNode);
-			searchingNode = searchingNode->m_pRight;
+			SwapValues(iter.GetNode()->m_pRight, iter.GetNode());
+			iter.Right();
 		}
 		// done sorting
 		else
@@ -288,121 +391,44 @@ inline void Priority_Queue<DataType>::SortOnDown(Node* pNode)
 template<typename DataType>
 inline void Priority_Queue<DataType>::SortOnUp(Node* pNode)
 {
-	Node* searchingNode = pNode;
-
-	// if node is bigger than maxElement
-	if (*searchingNode > *m_pRoot)
-	{
-		SwapValues(searchingNode, m_pRoot);
-		m_maxElement = searchingNode->m_value;
-	}
+	Iterator iter = pNode;
 
 	// Until it reachs the root
-	while (searchingNode->m_pParent != nullptr)
+	while (iter.GetNode()->m_pParent != nullptr)
 	{
 		// if the value is bigger than parents value
-		if (*searchingNode > *searchingNode->m_pParent)
-			SwapValues(searchingNode, searchingNode->m_pParent);
+		if (*iter.GetNode() > *iter.GetNode()->m_pParent)
+			SwapValues(iter.GetNode(), iter.GetNode()->m_pParent);
 		else
 			break;
 
-		searchingNode = searchingNode->m_pParent;
+		iter.Parent();
+	}
+}
+
+#if _DEBUG
+template<typename DataType>
+inline void Priority_Queue<DataType>::DebugPrint(Node* pNode_a, Node* pNode_b)
+{
+	system("cls");
+
+
+	if (pNode_b)
+	{
+		std::cout << "Node Change [" << pNode_a->m_key << "] "<< pNode_a->m_value << "   <--->   " << "[" << pNode_b->m_key << "]" << pNode_b->m_value << "\n\n";
+	}
+	else if(pNode_a)
+	{
+		std::cout << "Node Insert [" << pNode_a->m_key << "] " << pNode_a->m_value << "\n\n";
+	}
+	else
+	{
+		std::cout << "Delete last node [" << m_size << "]\n\n";
 	}
 
+	Print();
+	system("pause");
 
 }
 
-
-/*
-
-template<typename DataType>
-class Priority_Queue
-{
-private:
-	struct Node
-	{
-	private:
-		size_t m_key;
-		DataType m_value;
-
-	public:
-		Node() = default;
-		Node(size_t key, DataType value) { m_key = key; m_value = value; }
-		~Node() { m_pParent = m_pLeft = m_pRight = nullptr; }
-		Node* m_pParent = nullptr;
-		Node* m_pLeft = nullptr;
-		Node* m_pRight = nullptr;
-
-		// Getters
-		size_t GetKey() const { return m_key; }
-		DataType GetValue() const { return m_value; }
-
-		// Setters
-		void SetValue(const DataType& value) { m_value = value; }
-	};
-
-public:
-	class Iterator
-	{
-		friend class Priority_Queue;
-
-	private:
-		Node* m_pNode = nullptr;
-
-	public:
-		Iterator() = default;
-		Iterator(Node* pNode)
-			: m_pNode(pNode)
-		{}
-
-		// Move to Next Node
-		void Parent() { m_pNode = m_pNode->m_pParent; }
-		void Left() { m_pNode = m_pNode->m_pLeft; }
-		void Right() { m_pNode = m_pNode->m_pRight; }
-
-		size_t GetKey() { m_pNode->GetKey(); }
-
-		// return node
-		Node* operator*() { return m_pNode; }
-
-	};
-
-	// Functions
-public:
-	Priority_Queue() = default;
-	~Priority_Queue() {};
-
-	Node* operator[](const size_t& index)
-	{
-		return Find(index);
-	}
-
-	void Insert(const DataType& value);
-	void Remove(const size_t& indexKey);
-
-private:
-
-
-
-	// Variables
-private:
-	Node* m_pRoot = nullptr;
-
-	size_t m_size = 0;
-};
-
-template<typename DataType>
-void Priority_Queue<DataType>::Insert(const DataType& value)
-{
-	++m_size;
-
-	Node* m_pNewNode;
-	Node* m_pParentNode = Find(m_size)->m_pParent;
-	
-	m_pNewNode = new Node(m_size, value);
-
-
-
-}
-*/
-
+#endif // DEBUG
